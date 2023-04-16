f039c0e6e1cac1550128b407aab437b715e029de
Vaibhav Shrivathsa <Vaibhav.Shrivathsa@gmail.com>
2023-04-16 17:12:17 -0500

    Created UITools File. Converted Tuple Math to Vector2 Math.

---
+++ UITools.py
@@ -0,0 +1,59 @@
+import pygame
+from pygame.math import Vector2
+
+
+class UI:
+    WINDOW_SIZE = Vector2(700, 700)
+    WINDOW_CENTER = WINDOW_SIZE / 2
+    WINDOW_MIN_COORD_SIZE = min(WINDOW_SIZE[0], WINDOW_SIZE[1])
+    pygame.font.init()
+    TEXT_FONT = pygame.font.SysFont('Lucida Console', 70)
+
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+
+
+class Text:
+    def __init__(self, text, color, pos, size):
+        self.surface = UI.TEXT_FONT.render(text, False, color)
+        text_width = self.surface.get_size()[0]
+
+        new_text_size = Vector2(self.surface.get_size()) * \
+                            (UI.WINDOW_SIZE[0] * size) / text_width
+        self.surface = pygame.transform.scale(self.surface, new_text_size)
+
+        pos = pos.elementwise() * UI.WINDOW_SIZE
+        new_text_size = Vector2(self.surface.get_size())
+        self.surface_top_left = pos - new_text_size / 2
+
+    def render(self, game):
+        game.screen.blit(self.surface, self.surface_top_left)
+
+
+class Button:
+    OUTER_BOX_SCALE = 3 / 2
+    OUTER_BOX_THICKNESS = 2
+
+    def __init__(self, text, color, hover_color, pos, size, phase_on_press):
+        self.color = color
+        self.text = Text(text, color, pos, size)
+        self.hover_text = Text(text, hover_color, pos, size)
+
+        pos = pos.elementwise() * UI.WINDOW_SIZE
+        text_size = Vector2(self.text.surface.get_size())
+
+        outer_box_size = text_size * Button.OUTER_BOX_SCALE
+        outer_box_top_left = pos - outer_box_size / 2
+        self.outer_box = pygame.Rect(outer_box_top_left, outer_box_size)
+
+        self.phase_on_press = phase_on_press
+
+    def render(self, game):
+        if self.outer_box.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(game.screen, self.color, self.outer_box)
+            self.hover_text.render(game)
+            if pygame.mouse.get_pressed()[0]:
+                game.game_phase = self.phase_on_press
+        else:
+            pygame.draw.rect(game.screen, self.color, self.outer_box, Button.OUTER_BOX_THICKNESS)
+            self.text.render(game)
--- a5.py
+++ a5.py
@@ -27,29 +27,18 @@ Complete the following:
 import math
 
 import pygame
+from pygame.math import Vector2
+
+import UITools
+from UITools import UI
+
 pygame.init()
 
-def add(tuple1, tuple2):
-    return tuple1[0] + tuple2[0], tuple1[1] + tuple2[1]
-def sub(tuple1, tuple2):
-    return tuple1[0] - tuple2[0], tuple1[1] - tuple2[1]
-def scale(tuple, scalar):
-    return tuple[0] * scalar, tuple[1] * scalar
-
-class UI:
-    WINDOW_SIZE = (700, 700)
-    pygame.font.init()
-    TITLE_FONT = pygame.font.SysFont('Lucida Console', 50)
-    TEXT_FONT = pygame.font.SysFont('Lucida Console', 30)
-
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-
 class GameGraphics:
-    ship_image_size = (100, 100)
+    ship_image_size = Vector2(100, 100)
     BLUE_SHIP_SURFACE = pygame.image.load("blue-ship.png")
 
-class GameState:
+class GameInfo:
     def __init__(self):
         self.screen = pygame.display.set_mode(UI.WINDOW_SIZE)
         pygame.display.set_caption("Astro Party Clone")
@@ -57,22 +46,22 @@ class GameState:
         self.game_phase = GamePhase.INTRO_MENU
 
 class GameMatchState:
-    HUMAN_START_POS = (8.0, -8.0)
-    HUMAN_START_VEL = (0.0, 0.0)
+    HUMAN_START_POS = Vector2(8, -8)
+    HUMAN_START_VEL = Vector2(0, 0)
     HUMAN_START_ANG = 90
-    CAMERA_START_POS = (0.0, 0.0)
+    CAMERA_START_POS = Vector2(0, 0)
     CAMERA_INIT_VIEW_SIZE = 10
 
     def __init__(self, game):
-        self.human = PlayerShip()
-        self.human.pos = GameMatchState.HUMAN_START_POS
-        self.human.vel = GameMatchState.HUMAN_START_VEL
-        self.human.angle = GameMatchState.HUMAN_START_ANG
-        self.ai = PlayerShip()
+        self.human = PlayerShip(GameMatchState.HUMAN_START_POS,
+                                GameMatchState.HUMAN_START_VEL,
+                                GameMatchState.HUMAN_START_ANG)
 
-        self.camera = Camera(game)
-        self.camera.center = GameMatchState.CAMERA_START_POS
-        self.camera.size_of_view = GameMatchState.CAMERA_INIT_VIEW_SIZE
+        #self.ai = PlayerShip()
+
+        self.camera = Camera(game,
+                             GameMatchState.CAMERA_START_POS,
+                             GameMatchState.CAMERA_INIT_VIEW_SIZE)
 
 class GamePhase:
     INTRO_MENU = 0
@@ -80,13 +69,10 @@ class GamePhase:
     GAME_EXITED = 2
 
 
-WINDOW_SIZE = (700, 700)
-
-
 def main():
-    game = GameState()
+    game = GameInfo()
     while game.game_phase != GamePhase.GAME_EXITED:
-        game.clock.tick(60)
+        game.clock.tick(120)
         if game.game_phase == GamePhase.INTRO_MENU:
             intro_menu_phase(game)
         elif game.game_phase == GamePhase.IN_MATCH:
@@ -98,31 +84,17 @@ def intro_menu_phase(game):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game.game_phase = GamePhase.GAME_EXITED
+
     game.screen.fill(UI.BLACK)
+    intro_menu_phase.play_button.render(game)
+    intro_menu_phase.title_text.render(game)
 
-    screen_center = scale(UI.WINDOW_SIZE, 1/2)
-    title = UI.TITLE_FONT.render("ASTRO WARS", False, UI.WHITE)
-    title_pos = add(screen_center, (0, -100))
-    game.screen.blit(title, dest=sub(title_pos, scale(title.get_size(), 1/2)))
 
-    play_text = UI.TEXT_FONT.render("PLAY", False, UI.WHITE)
-    play_text_pos = add(screen_center, (0, 0))
-
-    play_box_size = scale(play_text.get_size(), 3/2)
-    play_top_left = sub(play_text_pos, scale(play_box_size, 1/2))
-
-    if play_top_left[0] <= pygame.mouse.get_pos()[0] <= play_top_left[0] + play_box_size[0] and \
-       play_top_left[1] <= pygame.mouse.get_pos()[1] <= play_top_left[1] + play_box_size[1]:
-        play_text = UI.TEXT_FONT.render("PLAY", False, UI.BLACK)
-        pygame.draw.rect(game.screen, UI.WHITE,
-                         pygame.Rect(play_top_left[0], play_top_left[1], play_box_size[0], play_box_size[1]))
-        game.screen.blit(play_text, dest=sub(play_text_pos, scale(play_text.get_size(), 1 / 2)))
-        if pygame.mouse.get_pressed()[0]:
-            game.game_phase = GamePhase.IN_MATCH
-    else:
-        game.screen.blit(play_text, dest=sub(play_text_pos, scale(play_text.get_size(), 1 / 2)))
-        pygame.draw.rect(game.screen, UI.WHITE,
-                         pygame.Rect(play_top_left[0], play_top_left[1], play_box_size[0], play_box_size[1]), 2)
+intro_menu_phase.play_button = UITools.Button("PLAY", UI.WHITE, UI.BLACK,
+                                              Vector2(0.5, 0.5), 1 / 7,
+                                              GamePhase.IN_MATCH)
+intro_menu_phase.title_text = UITools.Text("ASTRO WARS", UI.WHITE,
+                                           Vector2(0.5, 0.5 - 1 / 7), 3 / 7)
 
 def game_match_phase(game):
     for event in pygame.event.get():
@@ -146,25 +118,27 @@ game_match_phase.match_state = None
 
 class PlayerShip:
     MAX_SPEED = 8
+    SHIP_ACCEL_MAGNITUDE = 10
+    TURNING_ANGULAR_VEL = 170
+
     MAX_SHOTS = 3
     SHOT_ANIM_ANGULAR_VEL = 5
-    TURNING_ANGULAR_VEL = 170
-    SHIP_SIZE = (1, 1)
 
-    def __init__(self):
-        self.pos = None
-        self.vel = None
-        self.accel = None
-        self.angle = None
+    SHIP_SIZE = Vector2(1, 1)
+
+    def __init__(self, pos, vel, angle):
+        self.pos = pos
+        self.vel = vel
+        self.accel = 0
+        self.angle = angle
         self.shots_left = PlayerShip.MAX_SHOTS
         self.shot_rotate_anim_angle = 0
 
     def pos_update(self, dt):
-        self.accel = (-math.sin(math.radians(self.angle)) * 10, math.cos(math.radians(self.angle)) * 8)
-        self.vel = add(self.vel, scale(self.accel, dt))
-        if self.vel[0] ** 2 + self.vel[1] ** 2 > PlayerShip.MAX_SPEED ** 2:
-            self.vel = scale(self.vel, PlayerShip.MAX_SPEED/math.sqrt(self.vel[0] ** 2 + self.vel[1] ** 2))
-        self.pos = add(self.pos, scale(self.vel, dt))
+        self.accel = Vector2.from_polar((PlayerShip.SHIP_ACCEL_MAGNITUDE, self.angle + 90))
+        self.vel = self.vel + (self.accel * dt)
+        self.vel.clamp_magnitude_ip(PlayerShip.MAX_SPEED)
+        self.pos = self.pos + (self.vel * dt)
 
     def take_in_player_input(self, dt):
         keys = pygame.key.get_pressed()
@@ -173,26 +147,25 @@ class PlayerShip:
 
 
 class Camera:
-    def __init__(self, game):
-        self.center = None
-        self.size_of_view = None
-        self.game: GameState = game
+    def __init__(self, game, center, size_of_view):
+        self.center = center
+        self.size_of_view = size_of_view
+        self.game: GameInfo = game
 
     def render_player_ship(self, ship: PlayerShip, ship_image):
-        ship_camera_disp = sub(ship.pos, self.center)
-        coord_to_pixel_factor = UI.WINDOW_SIZE[0]/(2 * self.size_of_view)
-        ship_pixel_disp = scale(ship_camera_disp, coord_to_pixel_factor)
-        ship_pixel_disp = (ship_pixel_disp[0], -ship_pixel_disp[1])
-        ship_pixel_pos = add(scale(UI.WINDOW_SIZE, 1/2), ship_pixel_disp)
+        ship_camera_disp = ship.pos - self.center
+        coord_to_pixel_factor = UI.WINDOW_MIN_COORD_SIZE/(2 * self.size_of_view)
+        ship_pixel_disp = ship_camera_disp * coord_to_pixel_factor
+        ship_pixel_disp.update(ship_pixel_disp[0], -ship_pixel_disp[1])
+        ship_pixel_pos = ship_pixel_disp + UI.WINDOW_SIZE / 2
 
-        ship_img_size = scale(PlayerShip.SHIP_SIZE, coord_to_pixel_factor)
-        ship_render_image = pygame.transform.scale(ship_image, scale(PlayerShip.SHIP_SIZE, coord_to_pixel_factor))
+        ship_render_image = pygame.transform.scale(ship_image, PlayerShip.SHIP_SIZE * coord_to_pixel_factor)
         ship_render_image = pygame.transform.rotate(ship_render_image, ship.angle)
 
-        render_img_size = ship_render_image.get_size()
-        ship_pixel_pos_offset = scale(render_img_size, 1/2)
+        # Pygame Renders Images With Top Left Corner Of Image At Given Position
+        offset = Vector2(ship_render_image.get_size()) / 2
 
-        self.game.screen.blit(ship_render_image, sub(ship_pixel_pos, ship_pixel_pos_offset))
+        self.game.screen.blit(ship_render_image, ship_pixel_pos - offset)
 
 
 if __name__ == '__main__':
